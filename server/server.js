a8e389e613a2abb1bffc3d8cfd5e042fe607f247
vchadala <vchadala@syr.edu>
2017-12-27 21:50:44 -0600

    added socket io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const path=require('path');
+const socketIO=require('socket.io');
 const express=require('express');
+const http=require('http');
+
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
 const app=express();
-
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath));
 
-app.get('/',(req, res) => {
-    res.send("Hello world");
-});
+io.on('connection',(socket) =>{
+    console.log("NEW USER CONNECTED");
 
-app.listen(3000, ()=>{
+socket.on('disconnect',()=>{
+    console.log("new user disconnected");
+});
+});
+server.listen(port, ()=>{
     console.log(`app is now ready on port ${port}`);
 });
